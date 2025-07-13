e249985cfa505bef0b185bf097293c240825523e
Mostafa <mostafamamdoh479@gmail.com>
2025-07-13 19:10:32 +0300

    Add files via upload

--- darkmode.js
+++ darkmode.js
@@ -1,11 +1,9 @@
-           // ========== SCRIPT JS========== //
 var icon =document.getElementById("icon");
 icon.onclick = function(){
     document.body.classList.toggle("darkmode");
     if(document.body.classList.contains("darkmode")){
-        icon.src ="assets/images/sun.png";
+        icon.src ="images/sun.png";
         }else{
-            icon.src ="assets/images/moon.png";
+            icon.src ="images/moon.png";
         }
 }
-           // ========== The code has expired ========== //
